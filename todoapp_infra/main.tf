d89b904812656a83af1dbef88a78b00bf506ab1e
DevOps Insiders <info@devopsinsiders.com>
2025-06-29 11:34:22 +0530

    updated code

--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -16,6 +16,7 @@ module "resource_group" {
   resource_group_location = "centralindia"
 }
 
+# This RG is created as part of JIRA-101 Ticket for Project TodoApp
 module "resource_group1" {
   source                  = "../modules/azurerm_resource_group"
   resource_group_name     = "rg-todoapp1"
